6195e8305fabf10183b16886963f53e7cf84c061
Ashish Wagle <Awagle4@gmail.com>
2022-12-14 11:36:49 +0545

    first commit

---
+++ resources/views/template.blade.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="heading">
+        <div class="info">Call Us: +977 61 402070 / 402071
+        </div>
+        <ul>
+            <li class="top-menu"><a href="https://ngn.org.np/downloads">Downloads</a></li>
+            <li class="top-menu1"><a href="https://ngn.org.np/downloads">Career</a></li>
+            <li class="top-menu2"><a href="https://ngn.org.np/downloads">Contact Us</a></li>
+        </ul>
+    </div>
+    <div class="logo">
+        <img src="{{asset('img/logo.png')}}" alt="Naulo Ghumti Nepal">
+    </div>
+    <div class="location">Pokhara-17,Sitapaila</div>
+    <div class="location1">Head Office: 061-402070, 061-402071<br>
+        Female Rehabilitation :<br>
+        061- 402069, 9745289578
+    </div>
+
+    <div class="location2">Prithvi Chowk</div>
+    <div class="location3">Linkages Project for HIV/STI Testingp
+        061-402068
+    </div>
+    </div>
+    <div class="navbar">
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About Us</a></li>
+            <li><a href="">Projects</a></li>
+            <li><a href="">News and events</a></li>
+            <li><a href="">Reports</a></li>
+            <li><a href="">Our team</a></li>
+            <li><a href="">Photo gallery</a></li>
+        </ul>
+    </div>
+
+    <div class="background"></div>
+    <div class="orangebox">
+        <div class="inner-box">
+            <h2 class="text-uppercase">News and Events</h2>
+            <ul class="news">
+                <li>Naulo Ghumti Nepal's 21st AGM & 11th Convention.</li>
+                <li> Table Tennis Basic Orientation to Drug Rehabilitation Personnel.</li>
+                <li> Interaction program on Gender Based Violence to mark 16 Days of Activism against Gender-Based Violence.</li>
+                <li> Joint Monitoring Visit by Nepal Government Home Ministry and District Administrative Office, Kaski</li>
+                <li> Relief support to Rural Municipalities during pandemic COVID-19</li>
+            </ul>
+        </div>
+    </div>
+    <div class="blackbox">
+        <h2 class="htext">We do work with for</h2>
+        <ul class=list-one>
+            <li>Current drug users, recovering users, their spouses and family.</li>
+            <li>HIV infected and affected people.</li>
+            <li>Female Sex Workers and their spouses.</li>
+            <li>Children, youth & women.</li>
+            <li>Poor and marginalized community people in rural and urban slum areas.</li>
+        </ul>
+
+    </div>
+
+    <p class="paragraph">Organisationally Naulo Ghumti–Nepal is a non profitable and non-governmental organisation [NGO] working in various districts across the country. It is registered in Kaski District Administration Office. It is affiliated with SWC and also approved by Drug Control Division under Ministry of Home Affairs.. It has a very good linkage and coordination with Nepal government agencies and civil society organizations at national, regional and district level as well.</p>
+
+    <div class="drugbox">
+        <div class="druglogo"></div>
+    </div>
+    <div class="drughead">Drug Treatment & Rehabilitation</div>
+    <div class="drugpara">Rehabilitation, or rehab, can be used to help a person recover from addictions, injuries, and even physical or mental illnesses.</div>
+    <div class="hivpara">HIV prevention might refer to practices done to prevent the spread of HIV/AIDS. HIV prevention practices may be done by individuals to protect "their own health".</div>
+
+    <div class="hivbox">
+        <div class="hivimg"></div>
+    </div>
+    <div class="hivtopic">HIV/AIDS Prevention</div>
+
+    <div class="combox">
+        <div class="comimg"></div>
+    </div>
+    <div class="community">Community Development</div>
+    <div class="compara">Community development is a way of strengthening civil society by prioritising the actions of communities, and their perspectives.</div>
+    <div style="position: absolute;
+                width: 1519px;
+                height: 300px;
+                left: 0px;
+                top: 1317px;
+
+                background: url(.jpg), #343434;
+                border-radius: 0px;">
+
+        <div class="address">ADDRESS</div>
+        <div class="alladdress">Naulo Ghumti Nepal<br>
+            Pokhara-17, Sitapaila, Nepal<br>
+            P.O Box : 387<br>
+            +977 61 402070 / 402071<br>
+            +977 61 402069<br>
+            nauloghumti@ngn.org.np
+        </div>
+
+        <div class="project">OUR PROJECT</div>
+
+        <div class="allproject">Integrated Community Development Project<br>
+            Counseling, Treatment and Drug Rehabilitation Center (CTRC)<br>
+            Suspension Bridge Program<br>
+            +977 61 402070 / 402071<br>
+            EpiC Project<br>
+            TT for New Life (TNL) Project
+        </div>
+        <div class="latest">LATEST NEWS</div>
+        <div class="alllatest">Naulo Ghumti Nepal's 21st AGM & 11th Convention.<br>
+            Table Tennis Basic Orientation to Drug Rehabilitation Personnel.<br>
+            Interaction program on Gender Based Violence to mark 16 Days of Activism against Gender-Based Violence.<br>
+            Joint Monitoring Visit by Nepal Government Home Ministry and District Administrative Office, Kaski<br>
+            Relief support to Rural Municipalities during pandemic COVID-19<br>
+        </div>
+
+        <h1 class="naulo">2022-naulo ghumti nepal </h1>
+    </div>
+
+</body>
+
+</html>
